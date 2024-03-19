694934140f2d09d5d0c2625f99860547b5ea9a2e
Elin Stridsberg <elin-stridsberg@hotmail.com>
2024-03-19 10:06:21 +0100

    env server done

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const app = express();
+require("dotenv").config();
 const cors = require('cors');
 const fs = require('fs').promises;
 const imageSchema = require("./schemas/image.schema")
+
+const app = express();
+const port = process.env.PORT || 3000
+
 
 app.use(express.json());
 app.use(cors());
